a8030537936f4d93b4b93f57c0eb1d01ba9db9c4
Just1ñ <86172739+Agus9217@users.noreply.github.com>
2026-08-14 16:09:28 -0300

    prueba

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,58 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import Image from 'next/image';
+import { useRef, useState } from 'react';
+import Webcam from 'react-webcam';
 
 export default function Home() {
+  const [imageSrc, setImageSrc] = useState<string | null>(
+    null,
+  );
+  const webcamRef = useRef<Webcam>(null);
+
+  const handleUserMediaError = (
+    error: string | DOMException,
+  ) => {
+    if (error instanceof DOMException) {
+      console.error(
+        'ERROR cámara:',
+        error.name,
+        error.message,
+      );
+    } else {
+      console.error('ERROR cámara:', error);
+    }
+  };
+
+  const capture = () => {
+    const screenshot = webcamRef.current?.getScreenshot();
+    if (screenshot) setImageSrc(screenshot);
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="flex">
+      <div>
+        <Webcam
+          audio={false}
+          height={360}
+          width={720}
+          ref={webcamRef}
+          onUserMedia={(stream) =>
+            console.log('OK, stream:', stream)
+          }
+          onUserMediaError={handleUserMediaError}
         />
-        <div className={styles.intro}>
-          <h1>
-            To get started, edit the{" "}
-            <code className={styles.code}>page.tsx</code> file.
-          </h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+        <button onClick={capture}>Capture photo</button>
+      </div>
+      {imageSrc && (
+        <Image
+          width={300}
+          height={300}
+          src={imageSrc}
+          alt="Capture"
+        />
+      )}
     </div>
   );
 }
